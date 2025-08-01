1af134237c15fc015fa79996acc1609dee200f3d
RichoM <richi.moran@gmail.com>
2025-08-01 11:56:23 -0300

    Increased default font size

--- src/_init.clj
+++ src/_init.clj
@@ -89,7 +89,7 @@
     (navbar file-name)
     (case file-name
       "index.html" document
-      [:div.container document])
+      [:div.container.fs-5 document])
     footer
     [:script {:src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
               :integrity "sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
